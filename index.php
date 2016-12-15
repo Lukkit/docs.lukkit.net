552ece77179be029dab5d857d2f6cc96a1adac8b
James Upjohn <jspartan250@gmail.com>
2016-12-15 17:03:33 +1300

    Fixed Mixed Content issue loading docs iframe

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
-		<style> 
+		<style>
 		  .sidebar {
 			position: fixed;
 			top: 0;
@@ -60,8 +60,7 @@
 			</ul>
 		</div>
 		<div class="page">
-			<iframe name="page" src="org"></iframe>
+			<iframe name="page" src="https://theartex.net/lukkit/docs/org/"></iframe>
 		</div>
 	</body>
 </html>
-
